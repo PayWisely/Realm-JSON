30a0e45251822b41e1c1bcede8b628aa12da9732
Josh Stern <josh@getwisely.com>
2016-05-21 16:56:43 -0400

    added

---
+++ RealmJSONFramework/RealmJSONFramework.h
@@ -0,0 +1,19 @@
+//
+//  RealmJSONFramework.h
+//  RealmJSONFramework
+//
+//  Created by Josh Stern on 5/21/16.
+//  Copyright © 2016 Wisely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmJSONFramework.
+FOUNDATION_EXPORT double RealmJSONFrameworkVersionNumber;
+
+//! Project version string for RealmJSONFramework.
+FOUNDATION_EXPORT const unsigned char RealmJSONFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmJSONFramework/PublicHeader.h>
+
+
